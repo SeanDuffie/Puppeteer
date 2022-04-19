6057d53b81d690a2e7388aa34c29870bdbb3a55a
= <=>
2022-04-19 17:06:28 -0500

    Adding Initial Files from Apache PHP on Pi

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+?>
+
+// HTML Lines
+<!DOCTYPE html>
+	<html>
+		<head>
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+            		<link rel="icon" href="data:,">
+		        // CSS to style the on/off buttons 
+            		// Feel free to change the background-color and font-size attributes to fit your preferences
+            		<style>
+				html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}
+				.button { background-color: #195B6A; border: none; color: white; padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}
+				.button2 {background-color: #77878A;}
+			</style>
+            		<meta http-equiv="refresh" content="5">
+		</head>
+            
+            // Web Page Heading
+            	<body>
+			<h1>ESP8266 Web Server</h1>
+		</body>
