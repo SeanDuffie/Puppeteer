69049f06c692109b0d01580a2bfcf8b248812b6d
Sean Duffie <duffiesean@gmail.com>
2022-08-27 14:54:38 -0500

    Add .htaccess to auto update insecure links; Replaced Flash video player with html (still doesn't work);

--- index.php
+++ index.php
@@ -28,17 +28,14 @@
     <body>
         <h1>ESP8266 Web Server</h1>
         <img src="read_image.php"/>
-        <OBJECT id=WindowsMediaPlayer1 width=320 height=264 hspace=5 vspace=5
-            classid=clsid:6BF52A52-394A-11D3-B153-00C04F79FAA6 border="5" align="middle" >
-            <param name="URL" value="http://www.my.domain/stream.php">
-
-            <EMBED type='application/x-mplayer2'
-                pluginspage='http://microsoft.com/windows/mediaplayer/en/download/'
-                id='mediaPlayer' name='mediaPlayer' displaysize='4' autosize='-1'
-                bgcolor='darkblue' showcontrols="true" showtracker='-1'
-                showdisplay='0' showstatusbar='-1' videoborder3d='-1' width="320" height="264"
-                src="read_video.php" autostart="0" designtimesp='5311'>
-            </EMBED>
-        </OBJECT>
+        <video width="1920" height="1080" controls autoplay>
+            <source src="resources/clip.avi" type="video/avi">
+            Sorry, your browser doesn't support the video element.
+        </video>
+        <!-- <video width="320" height="240" controls autoplay>
+            <object data="resources/clip.avi" width="320" height="240">
+                <embed width="320" height="240" src="resources/clip.avi">
+            </object>
+        </video> -->
     </body>
 </html>
