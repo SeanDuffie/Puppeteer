33c85f9657bb96795ca951f74ab84d52e762e6d0
Sean Duffie <duffiesean@gmail.com>
2023-01-09 22:01:13 -0600

    Button now works Sends the proper curl for each button Added More documentation Added todo list Major cleanup

--- curl.php
+++ curl.php
@@ -2,22 +2,14 @@
 // Sends curl GET to the esp32
 
 function get($pin, $state) {
-
     $ch = curl_init();
-    $fp = fopen("example_homepage.txt", "w");
 
-    echo "http://192.168.0.177/$pin/$state";
     curl_setopt($ch, CURLOPT_URL, "http://192.168.0.177/$pin/$state");
 
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $server_output = curl_exec($ch);
-    
-    if(curl_error($ch)) {
-        fwrite($fp, curl_error($ch));
-    }
 
     curl_close($ch);
-    fclose($fp);
 
     return $server_output;
 }
--- index.php
+++ index.php
@@ -1,129 +1,91 @@
 <?php
-// https://docs.php.earth/faq/misc/structure/
+/** https://docs.php.earth/faq/misc/structure/
+ *  TODO:
+ *  - Acquire Pin state from node directly, not initialized here
+ *  - Acquire list of available pins from node as well
+ *  - Allow for different IPs/URLs to be contacted if there are multiple nodes
+ *  - Create a list of node IPs somehow (brainstorm below)
+ *      - Scan a list of all devices on the local network
+ *      - Add a text form to the home page for appending to the list of devices
+ *      - Read in the list of devices from an external txt file
+ *  - Have a separate page for each device using a nav panel
+ */
     include "./curl.php";
 
     $APP = "Coffee Maker";
     $BACK = "#000000";
     $COLOR = "#800000";
     $TEXT = "#FFFFFF";
+
+    /** FIXME: These pin values should ideally not be hardcoded, maybe receive a list from the node **/
     $output0 = 5;
     $output1 = 32;
     $output2 = 33;
     $output3 = 25;
-    $output0State = "ON";
+
+    /** FIXME: I need these variables to be read from the nodes directly, not through  **/
+    $output0State = "OFF";
     $output1State = "OFF";
     $output2State = "OFF";
     $output3State = "OFF";
 
-    if(array_key_exists('button1', $_POST)) {
-        get(1, "ON");
-    }
-    else if(array_key_exists('button2', $_POST)) {
-        get(1, "ON");
-    }
+    echo "<!DOCTYPE html><html>\n";
+        echo "<head>\n";
+            echo "<title> Smart-Press | $APP </title>\n";
+            echo "<meta name='viewport' content='width=device-width, initial-scale=1'>\n";
+            echo "<link rel='icon' href='data:,'>\n";
 
-echo "<!DOCTYPE html><html>";
-    echo "<head>";
-        echo "<title> Smart-Press | $APP </title>";
-        echo "<meta name='viewport' content='width=device-width, initial-scale=1'>";
-        echo "<link rel='icon' href='data:,'>";
+            echo "<style>\n";
+            echo "html { font-family: Helvetica; color: $TEXT; display: inline-block; margin: 0px auto; text-align: center; }\n";
+            echo ".bg { background-color: $BACK; }\n";
+            echo ".ON { background-color: $COLOR; border: none; color: $TEXT; padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer; }\n";
+            echo ".OFF { background-color: $TEXT; border: none; color: $COLOR; padding: 16px 32px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer; }\n";
+            echo "</style>\n";
 
-        echo "<style>";
-        echo "html { font-family: Helvetica; color: $TEXT; display: inline-block; margin: 0px auto; text-align: center; }";
-        echo ".bg { background-color: $BACK; }";
-        echo ".ON { background-color: $COLOR; border: none; color: $TEXT; padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer; }";
-        echo ".OFF { background-color: $TEXT; border: none; color: $COLOR; padding: 16px 32px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer; }";
-        echo "</style>";
+            echo "<meta http-equiv='refresh' content='20'>\n";
+        echo "</head>\n";
 
-        echo "<meta http-equiv='refresh' content='20'>";
-    echo "</head>";
-
-    echo "<body class='bg'>";
-        echo "<h1>RPi Web Server</h1>";
+        echo "<body class='bg'>\n";
+            echo "<h1>RPi Web Server</h1>\n";
         
-        if (isset($_POST[$output0])) {
-            if ($_POST[$output0] == "ON") {
-                echo "output0 ON";
-                $output0State = "ON";
-                get(0, "ON");
-            } elseif ($_POST[$output0] == "OFF") {
-                echo "output0 OFF";
-                $output0State = "OFF";
-                get(0, "OFF");
-            }
-        }
-        if (isset($_POST[$output1])) {
-            if ($_POST[$output1] == "ON") {
-                echo "output1 ON";
-                $output1State = "ON";
-                get(1, "ON");
-            } elseif ($_POST[$output1] == "OFF") {
-                echo "output1 OFF";
-                $output1State = "OFF";
-                get(1, "OFF");
-            }
-        }
-        if (isset($_POST[$output2])) {
-            if ($_POST[$output2] == "ON") {
-                echo "output2 ON";
-                $output2State = "ON";
-                get(2, "ON");
-            } elseif ($_POST[$output2] == "OFF") {
-                echo "output2 OFF";
-                $output2State = "OFF";
-                get(2, "OFF");
-            }
-        }
-        if (isset($_POST[$output3])) {
-            if ($_POST[$output3] == "ON") {
-                echo "output3 ON";
-                $output3State = "ON";
-                get(3, "ON");
-            } elseif ($_POST[$output3] == "OFF") {
-                echo "output3 OFF";
-                $output3State = "OFF";
-                get(3, "OFF");
-            }
-        }
+    /** Handle Button Inputs **/
+    if (array_key_exists($output0, $_POST)) {
+        if ($output0State == "ON") { $output0State = "OFF"; }
+        elseif ($output0State == "OFF") { $output0State = "ON"; }
+        else { echo "ERROR WITH $output0!\n"; }
+        get(0, $output0State);
+    }
+    else if (array_key_exists($output1, $_POST)) {
+        if ($output1State == "ON") { $output1State = "OFF"; }
+        elseif ($output1State == "OFF") { $output1State = "ON"; }
+        else { echo "ERROR WITH $output1!\n"; }
+        get(1, $output1State);
+    }
+    else if (array_key_exists($output2, $_POST)) {
+        if ($output2State == "ON") { $output2State = "OFF"; }
+        elseif ($output2State == "OFF") { $output2State = "ON"; }
+        else { echo "ERROR WITH $output2!\n"; }
+        get(2, $output2State);
+    }
+    else if (array_key_exists($output3, $_POST)) {
+        if ($output3State == "ON") { $output3State = "OFF"; }
+        elseif ($output3State == "OFF") { $output3State = "ON"; }
+        else { echo "ERROR WITH $output3!\n"; }
+        get(3, $output3State);
+    }
 
-        echo "<form  method='post'>";
-            echo "<input type='button' class=$output0State name='$output0' value='$output0'>";
-            echo "<input type='button' class=$output1State name='$output1' value='$output1'>";
-            echo "<input type='button' class=$output2State name='$output2' value='$output2'>";
-            echo "<input type='button' class=$output3State name='$output3' value='$output3'>";
-        echo "</form>";
+    /** Display Button Forms to HTML */
+    echo "<form method='post'>\n";
+        echo "\t<p>GPIO $output0 - Currently $output0State</p>\n";
+        echo "\t<p><input type='submit' class=$output0State name='$output0' value='$output0State'/></p>\n";
+        echo "\t<p>GPIO $output1 - Currently $output1State</p>\n";
+        echo "\t<p><input type='submit' class=$output1State name='$output1' value='$output1State'/></p>\n";
+        echo "\t<p>GPIO $output2 - Currently $output2State</p>\n";
+        echo "\t<p><input type='submit' class=$output2State name='$output2' value='$output2State'/></p>\n";
+        echo "\t<p>GPIO $output3 - Currently $output3State</p>\n";
+        echo "\t<p><input type='submit' class=$output3State name='$output3' value='$output3State'/></p>\n";
+    echo "</form>\n";
 
-        echo "<p>$_POST[$output3]</p>";
-
-            // echo "<p>GPIO $output3 - Currently $output3State</p>";
-            // if ($output3State="OFF")
-            //     get(3, "ON");
-            //     echo "<p><a href=get(3,\"ON\")><button class=\"button1\">ON</button></a>";
-            // else
-            //     get(3, "OFF");
-            //     echo "<p><a href=get(3,\"OFF\")><button class=\"button0\">OFF</button></a></p>";
-
-            // // echo "<p>GPIO $output2 - Currently $output2State</p>";
-            // if ($output2State="OFF")
-            //     get(2, "ON");
-            //     echo "<p><a href=get(2,\"ON\")><button class=\"button1\">ON</button></a>";
-            // else
-            //     get(2, "OFF");
-            //     echo "<p><a href=\"get(2,\"OFF\")><button class=\"button0\">OFF</button></a></p>";
-
-            // echo "<p>GPIO $output1 - Currently $output1State</p>";
-            // if ($output1State="OFF")
-            //     get(1, "ON");
-            //     echo "<p><a href=get(1,\"ON\")><button class=\"button1\">ON</button></a>";
-            // else
-            //     get(1, "OFF");
-            //     echo "<p><a href=get(1,\"OFF\")><button class=\"button0\">OFF</button></a></p>";
-
-            // echo "<p>GPIO $output0 - Currently $output0State</p>";
-            // if ($output0State="OFF")
-            //     get(0, "ON");
-            //     echo "<p><a href=get(0,\"ON\")><button class=\"button1\">ON</button></a>";
-            // else
-            //     get(0, "OFF");
-            //     echo "<p><a href=get(0,\"OFF\")><button class=\"button0\">OFF</button></a></p>";
+    echo "<script type='text/javascript'>\n";
+    echo "</script>\n";
 ?>
