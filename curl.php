f856f3482c8569dd0062451bf9d97bfd713d0839
Sean Duffie <duffiesean@gmail.com>
2023-01-09 22:17:54 -0600

    Added URL variable input for curl Fixed indentation Properly closed body and html

--- curl.php
+++ curl.php
@@ -1,10 +1,10 @@
 <?php
 // Sends curl GET to the esp32
 
-function get($pin, $state) {
+function get($url, $pin, $state) {
     $ch = curl_init();
 
-    curl_setopt($ch, CURLOPT_URL, "http://192.168.0.177/$pin/$state");
+    curl_setopt($ch, CURLOPT_URL, "$url/$pin/$state");
 
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $server_output = curl_exec($ch);
--- index.php
+++ index.php
@@ -17,6 +17,9 @@
     $COLOR = "#800000";
     $TEXT = "#FFFFFF";
 
+    /** FIXME: Add a way for multiple urls handled separately **/
+    $URL = "http://192.168.0.177";
+
     /** FIXME: These pin values should ideally not be hardcoded, maybe receive a list from the node **/
     $output0 = 5;
     $output1 = 32;
@@ -34,58 +37,58 @@
             echo "<title> Smart-Press | $APP </title>\n";
             echo "<meta name='viewport' content='width=device-width, initial-scale=1'>\n";
             echo "<link rel='icon' href='data:,'>\n";
-
             echo "<style>\n";
-            echo "html { font-family: Helvetica; color: $TEXT; display: inline-block; margin: 0px auto; text-align: center; }\n";
-            echo ".bg { background-color: $BACK; }\n";
-            echo ".ON { background-color: $COLOR; border: none; color: $TEXT; padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer; }\n";
-            echo ".OFF { background-color: $TEXT; border: none; color: $COLOR; padding: 16px 32px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer; }\n";
+                echo "html { font-family: Helvetica; color: $TEXT; display: inline-block; margin: 0px auto; text-align: center; }\n";
+                echo ".bg { background-color: $BACK; }\n";
+                echo ".ON { background-color: $COLOR; border: none; color: $TEXT; padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer; }\n";
+                echo ".OFF { background-color: $TEXT; border: none; color: $COLOR; padding: 16px 32px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer; }\n";
             echo "</style>\n";
-
             echo "<meta http-equiv='refresh' content='20'>\n";
         echo "</head>\n";
 
         echo "<body class='bg'>\n";
             echo "<h1>RPi Web Server</h1>\n";
         
-    /** Handle Button Inputs **/
-    if (array_key_exists($output0, $_POST)) {
-        if ($output0State == "ON") { $output0State = "OFF"; }
-        elseif ($output0State == "OFF") { $output0State = "ON"; }
-        else { echo "ERROR WITH $output0!\n"; }
-        get(0, $output0State);
-    }
-    else if (array_key_exists($output1, $_POST)) {
-        if ($output1State == "ON") { $output1State = "OFF"; }
-        elseif ($output1State == "OFF") { $output1State = "ON"; }
-        else { echo "ERROR WITH $output1!\n"; }
-        get(1, $output1State);
-    }
-    else if (array_key_exists($output2, $_POST)) {
-        if ($output2State == "ON") { $output2State = "OFF"; }
-        elseif ($output2State == "OFF") { $output2State = "ON"; }
-        else { echo "ERROR WITH $output2!\n"; }
-        get(2, $output2State);
-    }
-    else if (array_key_exists($output3, $_POST)) {
-        if ($output3State == "ON") { $output3State = "OFF"; }
-        elseif ($output3State == "OFF") { $output3State = "ON"; }
-        else { echo "ERROR WITH $output3!\n"; }
-        get(3, $output3State);
-    }
+            /** Handle Button Inputs **/
+            if (array_key_exists($output0, $_POST)) {
+                if ($output0State == "ON") { $output0State = "OFF"; }
+                elseif ($output0State == "OFF") { $output0State = "ON"; }
+                else { echo "ERROR WITH $output0!\n"; }
+                get($URL, 0, $output0State);
+            }
+            else if (array_key_exists($output1, $_POST)) {
+                if ($output1State == "ON") { $output1State = "OFF"; }
+                elseif ($output1State == "OFF") { $output1State = "ON"; }
+                else { echo "ERROR WITH $output1!\n"; }
+                get($URL, 1, $output1State);
+            }
+            else if (array_key_exists($output2, $_POST)) {
+                if ($output2State == "ON") { $output2State = "OFF"; }
+                elseif ($output2State == "OFF") { $output2State = "ON"; }
+                else { echo "ERROR WITH $output2!\n"; }
+                get($URL, 2, $output2State);
+            }
+            else if (array_key_exists($output3, $_POST)) {
+                if ($output3State == "ON") { $output3State = "OFF"; }
+                elseif ($output3State == "OFF") { $output3State = "ON"; }
+                else { echo "ERROR WITH $output3!\n"; }
+                get($URL, 3, $output3State);
+            }
 
-    /** Display Button Forms to HTML */
-    echo "<form method='post'>\n";
-        echo "\t<p>GPIO $output0 - Currently $output0State</p>\n";
-        echo "\t<p><input type='submit' class=$output0State name='$output0' value='$output0State'/></p>\n";
-        echo "\t<p>GPIO $output1 - Currently $output1State</p>\n";
-        echo "\t<p><input type='submit' class=$output1State name='$output1' value='$output1State'/></p>\n";
-        echo "\t<p>GPIO $output2 - Currently $output2State</p>\n";
-        echo "\t<p><input type='submit' class=$output2State name='$output2' value='$output2State'/></p>\n";
-        echo "\t<p>GPIO $output3 - Currently $output3State</p>\n";
-        echo "\t<p><input type='submit' class=$output3State name='$output3' value='$output3State'/></p>\n";
-    echo "</form>\n";
+            /** Display Button Forms to HTML */
+            echo "<form method='post'>\n";
+                echo "\t<p>GPIO $output0 - Currently $output0State</p>\n";
+                echo "\t<p><input type='submit' class=$output0State name='$output0' value='$output0State'/></p>\n";
+                echo "\t<p>GPIO $output1 - Currently $output1State</p>\n";
+                echo "\t<p><input type='submit' class=$output1State name='$output1' value='$output1State'/></p>\n";
+                echo "\t<p>GPIO $output2 - Currently $output2State</p>\n";
+                echo "\t<p><input type='submit' class=$output2State name='$output2' value='$output2State'/></p>\n";
+                echo "\t<p>GPIO $output3 - Currently $output3State</p>\n";
+                echo "\t<p><input type='submit' class=$output3State name='$output3' value='$output3State'/></p>\n";
+            echo "</form>\n";
 
-    echo "<script type='text/javascript'>\n";
-    echo "</script>\n";
+            echo "<script type='text/javascript'>\n";
+            echo "</script>\n";
+        echo "</body>\n";
+    echo "</html>\n";
 ?>
