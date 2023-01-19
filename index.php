d96da3e1448042a6efa37714fea448545a462e7a
Sean Duffie <duffiesean@gmail.com>
2023-01-18 22:13:57 -0600

    Buttons reformatted. Config and Reset buttons added

--- index.php
+++ index.php
@@ -17,6 +17,22 @@
     $COLOR = "#800000";
     $TEXT = "#FFFFFF";
 
+
+    $HEAD = "<!DOCTYPE html><html>
+    <head>
+        <title> Smart-Press | $APP </title>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <link rel='icon' href='data:,'>
+        <link href='test.css' rel='stylesheet' type='text/css'/>
+        <style>
+            html { font-family: Helvetica; color: $TEXT; display: inline-block; margin: 0px auto; text-align: center; }
+            .bg { background-color: $BACK; }
+            .ON { background-color: $COLOR; border: none; color: $TEXT; padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer; }
+            .OFF { background-color: $TEXT; border: none; color: $COLOR; padding: 16px 32px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer; }
+            </style>
+        <meta http-equiv='refresh' content='20'>
+    </head>";
+
     /** FIXME: Add a way for multiple urls handled separately **/
     $URL = "http://192.168.0.177";
 
@@ -32,20 +48,7 @@
     $output2State = getPin($URL, $output2);
     $output3State = getPin($URL, $output3);
 
-    echo "<!DOCTYPE html><html>
-    <head>
-        <title> Smart-Press | $APP </title>
-        <meta name='viewport' content='width=device-width, initial-scale=1'>
-        <link rel='icon' href='data:,'>
-        <link href='test.css' rel='stylesheet' type='text/css'/>
-        <style>
-            html { font-family: Helvetica; color: $TEXT; display: inline-block; margin: 0px auto; text-align: center; }
-            .bg { background-color: $BACK; }
-            .ON { background-color: $COLOR; border: none; color: $TEXT; padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer; }
-            .OFF { background-color: $TEXT; border: none; color: $COLOR; padding: 16px 32px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer; }
-            </style>
-        <meta http-equiv='refresh' content='20'>
-    </head>
+    echo "$HEAD
     <body class='bg'>
         <h1>RPi Web Server</h1>\n";
 
@@ -55,36 +58,77 @@
         elseif (str_contains($output0State, "OFF")) { $output0State = "ON"; }
         else { echo "ERROR WITH $output0!"; }
         setPin($URL, $output0, $output0State);
-    }
-    else if (array_key_exists($output1, $_POST)) {
+    } else if (array_key_exists($output1, $_POST)) {
         if (str_contains($output1State, "ON")) { $output1State = "OFF"; }
         elseif (str_contains($output1State, "OFF")) { $output1State = "ON"; }
         else { echo "ERROR WITH $output1!"; }
         setPin($URL, $output1, $output1State);
-    }
-    else if (array_key_exists($output2, $_POST)) {
+    } else if (array_key_exists($output2, $_POST)) {
         if (str_contains($output2State, "ON")) { $output2State = "OFF"; }
         elseif (str_contains($output2State, "OFF")) { $output2State = "ON"; }
         else { echo "ERROR WITH $output2!"; }
         setPin($URL, $output2, $output2State);
-    }
-    else if (array_key_exists($output3, $_POST)) {
+    } else if (array_key_exists($output3, $_POST)) {
         if (str_contains($output3State, "ON")) { $output3State = "OFF"; }
         elseif (str_contains($output3State, "OFF")) { $output3State = "ON"; }
         else { echo "ERROR WITH $output3!"; }
         setPin($URL, $output3, $output3State);
+    } else if (array_key_exists("Config", $_POST)) {
+        echo getPin($URL, "config");
+    } else if (array_key_exists("Reset", $_POST)) {
+        echo getPin($URL, "rst");
     }
 
     /** Display Button Forms to HTML */
     echo "\t<form method='post'>
         <p>GPIO $output0 - Currently $output0State</p>
-        <p><input class=$output0State type='submit' name='$output0' value='$output0State'/></p>
+        <p>
+            <label class='switch switch-slide'>
+                <input class='switch-input' type='submit' name='$output0' value='$output0State'/>
+                <span class='switch-label' data-on='ON' data-off='OFF'></span>
+                <span class='switch-handle'></span>
+            </label>
+        </p>
         <p>GPIO $output1 - Currently $output1State</p>
-        <p><input type='submit' class=$output1State name='$output1' value='$output1State'/></p>
+        <p>
+            <label class='switch switch-slide'>
+                <input class='switch-input' type='submit' name='$output1' value='$output1State'/>
+                <span class='switch-label' data-on='ON' data-off='OFF'></span>
+                <span class='switch-handle'></span>
+            </label>
+        </p>
         <p>GPIO $output2 - Currently $output2State</p>
-        <p><input type='submit' class=$output2State name='$output2' value='$output2State'/></p>
+        <p>
+            <label class='switch switch-slide'>
+                <input class='switch-input' type='submit' name='$output2' value='$output2State'/>
+                <span class='switch-label' data-on='ON' data-off='OFF'></span>
+                <span class='switch-handle'></span>
+            </label>
+        </p>
         <p>GPIO $output3 - Currently $output3State</p>
-        <p><input type='submit' class=$output3State name='$output3' value='$output3State'/></p>
+        <p>
+            <label class='switch switch-slide'>
+                <input class='switch-input' type='submit' name='$output3' value='$output3State'/>
+                <span class='switch-label' data-on='ON' data-off='OFF'></span>
+                <span class='switch-handle'></span>
+            </label>
+        </p>
+        <p>Open Config Menu</p>
+        <p>
+            <label class='switch switch-slide'>
+                <input class='switch-input' type='submit' name='Config' value='Config'/>
+                <span class='switch-label' data-on='ON' data-off='OFF'></span>
+                <span class='switch-handle'></span>
+            </label>
+        </p>
+        <p>Reset Wifi Config</p>
+        <p>
+            <label class='switch switch-slide'>
+                <input class='switch-input' type='submit' name='Reset' value='Reset'/>
+                <span class='switch-label' data-on='ON' data-off='OFF'></span>
+                <span class='switch-handle'></span>
+            </label>
+        </p>
     </form>\n";
 
     // header("Content-type: text/plain");
